fcd1b656e439be508a54d6e718e7499a705f7870
unknown <elannugiferdiyanto@gmail.com>
2021-10-25 18:09:25 +0700

    first commit

---
+++ app/src/main/java/com/example/prak4_1918063_elannugiferdiyanto/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.prak4_1918063_elannugiferdiyanto;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private MahasiswaAdapter adapter;
+    private ArrayList<Mahasiswa> mahasiswaArrayList;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        addData();
+        recyclerView =
+                (RecyclerView)findViewById(R.id.recycleview);
+        adapter = new MahasiswaAdapter(mahasiswaArrayList);
+        RecyclerView.LayoutManager layoutManager = new
+                LinearLayoutManager(MainActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+    }
+    private void addData() {
+        mahasiswaArrayList = new ArrayList<>();
+        mahasiswaArrayList.add(new Mahasiswa("Elan Nugi Ferdiyanto", "1918063",
+                "2019"));
+        mahasiswaArrayList.add(new Mahasiswa("Abi", "1118002",
+                "2011"));
+        mahasiswaArrayList.add(new Mahasiswa("Yudis", "1118031",
+                "2011"));
+    }
+}
